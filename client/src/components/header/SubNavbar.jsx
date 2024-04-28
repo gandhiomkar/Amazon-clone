c65773ed0628070529de087b59b9ea96f0a2920a
Omkar Gandhi <gandhioms16@gmail.com>
2024-04-28 10:42:46 +0530

    initial commit

--- client/src/components/header/SubNavbar.jsx
+++ client/src/components/header/SubNavbar.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import './Navbar.css';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import subnav from './subnav.jpg';
+import React from "react";
+import "./Navbar.css";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import subnav from "./subnav.jpg";
+import { Link } from "react-router-dom";
 
 const SubNavbar = () => {
   return (
     <div className="sub-nav">
-
       <div className="left">
-        <a href='' className="left-item all">
+        <a href="" className="left-item all">
           <MenuOutlinedIcon id="hamburger" /> All
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           Best Sellers
         </a>
-        <a href='' className="left-item">
-          Mobiles
-        </a>
-        <a href='' className="left-item">
+        <Link to="/cellphones"> Mobile </Link>
+        <a href="" className="left-item">
           Customer Services
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           Today's Deals
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           Fashion
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           Electronics
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           Home & Kitchen
         </a>
-        <a href='' className="left-item">
+        <a href="" className="left-item">
           New Releases
         </a>
       </div>
@@ -42,9 +40,8 @@ const SubNavbar = () => {
           <img src={subnav} alt="Download App"></img>
         </a>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default SubNavbar;
